47b2e5336c768934299db15ac90f0aeed85d7d67
Mafaz03 <mohdmafaz200303@gmail.com>
2026-04-21 12:48:44 +0530

    somechanges

--- cholesky_serial.c
+++ cholesky_serial.c
@@ -74,6 +74,9 @@ int main()
 {
   TYPE a[N][N];
 
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
   initmult(a);
   
   printf("Before:\n");
@@ -83,6 +86,15 @@ int main()
 
   printf("\nAfter:\n");
   printLower(a);
+
+  clock_gettime(CLOCK_MONOTONIC, &end);
+  double time_taken = (end.tv_sec - start.tv_sec) +
+                      (end.tv_nsec - start.tv_nsec) / 1e9;
+  
+
+  printf("\nResult (Lower Triangular L):\n");
+
+  printf("\n\nExecution time: %f seconds\n", time_taken);
 
   return 0;
 }
